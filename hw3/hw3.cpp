8892ea8203a0b4aa4ac3575c3f3738acd3632883
Andy Tran <andydongtran@outlook.com>
2018-10-01 03:34:49 -0400

    tried to make floats and fix output, but ended up not being able to input anything

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -23,41 +23,42 @@ const int officey = 1000;
 //nextDist = initialDist in an equation
 
 //prints out the distance behind the given distance value, if it's over a mile, than not only will it print the amount of feet, it will print the amount of miles
-int DistOut(int dist, std::string state) {
-    if (dist > 5280) {
-        if ((dist % 5280) != 0) {
-            float mdist = ((static_cast<float>(dist))/5280); //results in how many miles from the feet given
-            std::cout << "   "
-                      << state
-                      << "                       "
-                      << dist
-                      << " (~"
-                      << mdist
-                      <<" miles)"
-                      << std::endl;
-        }
+int DistOut(float dist, std::string state) {
+	if ((static_cast<int>(dist) % 5280) != 0) {
+		std::cout << "   "
+		          << state
+		          << "                 "
+		          << dist << std::endl;
     } else {
-        std::cout << "   "
-                  << state
-                  << "                       "
-                  << dist << std::endl;
+    	float mdist = (dist/static_cast<float>(5280)); //results in how many miles from the feet given
+    	std::cout << "   "
+    	          << state
+    	          << "                 "
+    	          << dist
+    	          << " (~"
+    	          << mdist
+    	          <<" miles)"
+    	          << std::endl;
     }
     return 0;
 }
 
 //method to find the distance from one point to another 
 float findDistance(int x1, int y1, int x2, int y2) {
-    float dist = std::sqrt(((x2-x1)^2)+((y2-y1)^2)); //distance formula
+    int x = (x2-x1)^2;
+    int y = (y2-y1)^2;
+    int xy = x + y;
+    float dist = std::sqrt(xy); //distance formula
     return dist;
 }
 
 //end of the statistics
-int finishstats(int nick, int home, int lost) {
-    float nickStat = nick/numWalk;
-    float homeStat = home/numWalk;
-    float lostStat = lost/numWalk;
+int finishstats(int office, int home, int lost) {
+    float officeStat = office/(static_cast<float>(numWalk));
+    float homeStat = home/(static_cast<float>(numWalk));
+    float lostStat = lost/(static_cast<float>(numWalk));
     std::cout<< "Run Simulation finished! Here is the simulation's statistics: " << "\n"
-             << "Frequency of getting to Nick's: " << nickStat << "\n"
+             << "Frequency of getting to Nick's: " << officeStat << "\n"
              << "Frequency of hitting home: " << homeStat << "\n"
              << "Frequency of just being completely lost: " << lostStat << std::endl;
     return 0;
@@ -70,6 +71,10 @@ int distnext(int iniDist) {
 }
 
 std::string changeDirection(std::string orientation, std::string dir) {
+    if((orientation == "home")) {
+        return "right";
+    }
+    
     if((orientation == "up") && (dir == "right")) { //up and turns right
        return "right"; 
     }
@@ -117,65 +122,76 @@ int main(int argc, char **argv)
     
     std::cout<<"\nEnded walk at          total dist traveled (ft)" <<std::endl;
     for (int i = 0; i < numWalk; i++) { //for loop to run up to 250 times
+    
     //home coordinates
     int cordx = 0;
     int cordy = 0;
     //orientation and distrance travelled
-    std::string orientation = "up";
-    int disttraveled = iniDist;
+    std::string orientation = "home";
+    //int disttraveled = iniDist;
     int homepoints = 0; //number of decision points that are near home
+    int lostcheck = 0;
+    int dist = iniDist;
         for(int j = 1; j < dMax; j++) { //loop for one walk to make decisions inside of
-            nDist = distnext(iniDist);
+        
+            nDist = distnext(dist);
+            dist = nDist;
             
             //turning left or right
             if(j % 7 == 0) {
                 //moves left
-                 disttraveled += nDist;
+                 //disttraveled += nDist;
                  orientation = changeDirection(orientation, "left");
-            } 
-            if(j % 7 != 0) {
-                //moves right
-                disttraveled += nDist;
-                orientation = changeDirection(orientation, "right");
+            } else {
+            	//moves right
+            	//disttraveled += nDist;
+            	orientation = changeDirection(orientation, "right");
+
             }
-            
-            //after change in direction, change coordinates
-            if((orientation == "up")) {
-                cordy += nDist;
-            }
-            if((orientation == "right")) {
-                cordx += nDist;
-            }
-            if((orientation == "left")) {
-                cordx -= nDist;
-            }
-            if((orientation == "down")) {
-                cordy -= nDist;
-            }
-            
             if(j == dMax) { //checks if it's at the end of the for loop and increments the counter
-                lost++;
-                DistOut(disttraveled, "lost");
+                lostcheck = 1;
+                //DistOut(disttraveled, "lost");
                 break;
-            }
-            
-            if((findDistance(cordx, cordy, officex, officey) <= 500)) { //checks if within 500 feet of Nick's office and increment counter
-                office++;
-                DistOut(disttraveled, "office");
-                break;
-            }
-            
-            if((findDistance(cordx, cordy, homex, homey) <= 50)) { //checks if distance from current location to home is within 50
-                homepoints++;
-                if((findDistance(cordx, cordy, homex, homey) <= 50) && (homepoints >= 5)) {
-                    home++;
-                    DistOut(disttraveled, "home");
-                    break;
-                }
             }
         }
+        //after change in direction, change coordinates based on orientation
+        if((orientation == "up")) {
+            cordy += nDist;
+        }
+        if((orientation == "right")) {
+            cordx += nDist;
+        }
+        if((orientation == "left")) {
+            cordx -= nDist;
+        }
+        if((orientation == "down")) {
+            cordy -= nDist;
+        }
+        
+        float disttraveled = findDistance(homex, homey, cordx, cordy);
+        if(lostcheck == 1) {
+            lost++;
+            DistOut(disttraveled, "lost");
+            lost = 0;
+        }
+        if((findDistance(officex, officey, cordx, cordy) <= 500)) { //checks if within 500 feet of Nick's office and increment counter
+            office++;
+            DistOut(disttraveled, "office");
+            //break;
+        }
+        
+        if((findDistance(homex, homey, cordx, cordy)) <= 50) { //checks if distance from current location to home is within 50
+            homepoints++;
+            if(((findDistance(homex, homey, cordx, cordy)) <= 50) && (homepoints >= 5)) {
+                home++;
+                DistOut(disttraveled, "home");
+                //break;
+            }
+        }
+        
         iniDist++;//increment for the next run ex: 21 for 1st walk, 22 for 2nd walk, 23, for 3rd walk...
     }
+    std::cout << office << " " << home << " " << lost << " " << std::endl;
     finishstats(office, home, lost);
 	return 0;
 }
