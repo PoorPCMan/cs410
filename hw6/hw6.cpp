9e5e27e91673ea49e0e53c66f3f191f92b8f8c22
Andy Tran <andydongtran@outlook.com>
2018-10-18 00:20:00 -0400

    [hw6/not-compilable] set seed for rng to be 37
    -will probably be using std::rand() and std::srand()
    
    NOTE: not truly random and probably will skew in a certain direction
    https://stackoverflow.com/q/12657962
    
    NOTE2: Reference for TODO
    https://www.quora.com/What-is-the-equivalent-of-array-length-used-in-Java-in-C++

--- hw6/hw6.cpp
+++ hw6/hw6.cpp
@@ -1,45 +1,26 @@
 #include <iostream>
 #include <cmath>
 #include <string>
-
-creatures creaturelist{ 
-    {"Wanda Walrus", 2, 6},
-    {"Stanley Sardine", 3, 1},
-    {"Sylvia Seahorse", 4, 2},
-    {"Janie Jellyfish", 1, 10},
-    {"Doris Dolphin", 8, 4},
-    {"Bob Blobfish", 1, 5},
-    {"Sammy Shark", 8, 4},
-    {"Walter Whale", 6, 2},
-    {"Stevie Salmon", 2, 3},
-    {"Sheila Shellfish", 1, 3},
-    {"Daniel Octopus", 3, 7},
-    {"Mark Herrings", 9, 5},
-    {"Bernie Tuna", 3, 5},
-    {"Oscar Tilapia", 5, 3}
-};
-
-std::String things_to_say = {
-    "Whazzup?",
-    "Duuude, totally love the horse head and human body combo!",
-    "Looking for a card (shark)?",
-    "Are you someone famous?",
-    "You look fishy to me; or are you horsing around?"
-    "Don't trust the salmon!!!",
-    "Here's some advice: the shark wants you to be his chum!",
-    "Better take some paper towels if you visit the blobfish",
-    "Hey man, got any sea-weed?",
-    "Let me give you my card...my phone number is on the back",
-    "I'd gladly pay you Tuesday for a lobster roll today.",
-    "Don't be so crabby!",
-    "Just keep swimming, swimmin, swimming, ...",
-    "You just keep sinking, sinking, sinking,..."
-}
+#include 'hw6_functions.h'
 
 int main(int argc, char **argv)
 {
-	//simulate this guys collecting
+	//data to hold his leave conditions
     int stacked_cards = 0;
-    int creatures = 0;
-    //
+    int different_creatures = 0;
+    
+    //uses 37 as the seed to generate numbers due to hw requirement
+    std::srand(37);
+    
+    //main loop
+    std::cout<<"Hans is starting his trip to Atlantis!"<<std::endl;
+    while(stacked_cards != 6 || different_creatures != 14) {
+        //generate a random creature BEFORE generating a random saying
+        creature c = generateCreature();
+        std::cout<<"Han just encountered a " << c.name << "!" <<std::endl;
+        generatureSaying();
+        if() {
+            
+        }
+    }
 }
--- hw6/hw6_functions.cpp
+++ hw6/hw6_functions.cpp
@@ -1,5 +1,55 @@
+#include <iostream>
+#include <iomanip>
+#include "hw6_functions.h"
 
+creatures creaturelist { 
+    {"Wanda Walrus", 2, 6},
+    {"Stanley Sardine", 3, 1},
+    {"Sylvia Seahorse", 4, 2},
+    {"Janie Jellyfish", 1, 10},
+    {"Doris Dolphin", 8, 4},
+    {"Bob Blobfish", 1, 5},
+    {"Sammy Shark", 8, 4},
+    {"Walter Whale", 6, 2},
+    {"Stevie Salmon", 2, 3},
+    {"Sheila Shellfish", 1, 3},
+    {"Daniel Octopus", 3, 7},
+    {"Mark Herrings", 9, 5},
+    {"Bernie Tuna", 3, 5},
+    {"Oscar Tilapia", 5, 3}
+};
 
-int setupCards() {
+std::String things_to_say = {
+    "Whazzup?",
+    "Duuude, totally love the horse head and human body combo!",
+    "Looking for a card (shark)?",
+    "Are you someone famous?",
+    "You look fishy to me; or are you horsing around?"
+    "Don't trust the salmon!!!",
+    "Here's some advice: the shark wants you to be his chum!",
+    "Better take some paper towels if you visit the blobfish",
+    "Hey man, got any sea-weed?",
+    "Let me give you my card...my phone number is on the back",
+    "I'd gladly pay you Tuesday for a lobster roll today.",
+    "Don't be so crabby!",
+    "Just keep swimming, swimmin, swimming, ...",
+    "You just keep sinking, sinking, sinking,..."
+}
+
+int arraysize() {
     
+}
+
+creature generateCreature() {
+    int max_limit = 14; //TODO: learn and write a sizeof array operator without using anything C++11 and above
+    int min_limit = 0;
+    int random_number = rand()%(max-min + 1) + min;
+    return creaturelist[random_number];
+}
+
+String generateSaying() {
+    int max_limit = 14 //TODO: learn and write a sizeof array operator without using anything C++11 and above
+    int min_limit = 0;
+    int random_number = rand()%(max-min + 1) + min;
+    return things_to_say[random_number];
 }
